b3e2d4cdb0b5d510af49c9b83613d6a7525e4e77
Carlo Santos <rcarlosantos89@gmail.com>
2023-12-21 09:33:49 -0600

    created boilerplate and notes for components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
 
 function App() {
-  return ;
+  return (
+    < section className='main-container' >
+      {/* nav
+          hero
+          benefits
+          integrated services
+          pricing model
+          faq
+          footer
+          */}
+    </section >
+  )
+
 }
 
 export default App;
